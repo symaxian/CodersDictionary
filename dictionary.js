c1c59a1349285fcac4844c56ac78d77ea339b7ce
Jonathon Reesor <jonathon.reesor@gmail.com>
2011-09-30 15:35:45 -0400

    Initial commit.

---
+++ dictionary.js
@@ -0,0 +1,480 @@
+var dict = [
+	{
+		words:[
+			['arg','argument',null],
+			['param','parameter',null]
+		],
+		desc:'An item passed into a function/method.',
+		tag:'Functions'
+	},
+	{
+		word:'class',
+		desc:'A template for an object.',
+		tag:'Objects'
+	},
+	{
+		words:[
+			'interface',
+			'protocol',
+			'specification'
+		],
+		desc:'A set of parameters that an object/class may comply with. If so then that object/class is said to implement that interface/protocol/specification.',
+		tag:'Objects'
+	},
+	{
+		words:[
+			'member',
+			['attr','attribute','Usually refers to metadata'],
+			['field','Usually refers to a variable'],
+			['prop','property','Usually refers to a variable']
+		],
+		desc:'A piece of data/code associated with a specific object.',
+		tag:'Objects'
+	},
+	{
+		word:['ptr','pointer',null],
+		desc:'Points to a location in memory.',
+		tag:'Memory'
+	},
+	{
+		word:['ref','reference',null],
+		desc:'Refers to another item.',
+		tag:'Reference'
+	},
+	{
+		words:[
+			'list',
+			'array',
+			'stack'
+		],
+		desc:'Common names for linear data structures.',
+		tag:'Data Structure'
+	},
+	{
+		word:['col','column',null],
+		desc:'Refers to the column of a table.',
+		tag:'Data Structure'
+	},
+	{
+		word:'row',
+		desc:'Refers to the row of a table.',
+		tag:'Data Structure'
+	},
+	{
+		words:[
+			'this',
+			'self'
+		],
+		desc:'A reference to the current scope.',
+		tag:'Reference'
+	},
+	{
+		words:[
+			['pos','position',null],
+			['origin','Usually from where the object "begins", such as the top-left corner on modern display coordinate systems']
+		],
+		desc:'An objects position.',
+		tag:'Visual'
+	},
+	{
+		words:[
+			'x',
+			['h','horz','horizontal',null]
+		],
+		desc:'An objects horizontal position on a 2d plane.',
+		tag:'Visual'
+	},
+	{
+		words:[
+			'y',
+			['v','vert','vertical',null]
+		],
+		desc:'An objects vertical position on a 2d plane.',
+		tag:'Visual'
+	},
+	{
+		words:[
+			'xScale',
+			['hScale','horizontalScale',null]
+		],
+		desc:'An objects horizontal scale.',
+		tag:'Visual'
+	},
+	{
+		words:[
+			'yScale',
+			['vScale','verticalScale',null]
+		],
+		desc:'An objects vertical scale.',
+		tag:'Visual'
+	},
+	{
+		words:[
+			'angle',
+			['r','rotation',null]
+		],
+		desc:'An objects rotation.',
+		tag:'Visual'
+	},
+	{
+		word:['id','identifier',null],
+		desc:'A unique identifier for an object.',
+		tag:'Reference'
+	},
+	{
+		words:[
+			'slot',
+			'element'
+		],
+		desc:'A set place for data in a structure.',
+		tag:'Data Structure'
+	},
+	{
+		words:[
+			['proc','process',null],
+			'task',
+			'routine',
+			'operation'
+		],
+		desc:'Some piece of executable code, something that runs.',
+		tag:'Etc'
+	},
+	{
+		words:[
+			'draw',
+			'paint',
+			'render',
+			'output'
+		],
+		desc:'Draw some image/shape onto a destination.',
+		tag:'Visual'
+	},
+	{
+		words:[
+			['cpy','copy',null],
+			'clone',
+			'duplicate'
+		],
+		tag:'Method Names'
+	},
+	{
+		word:['var','variable',null],
+		tag:'Reference',
+		desc:'A piece of data.'
+	},
+	{
+		words:[
+			['i','index','Usually a number'],
+			['itr','iterator','Usually an object']
+		],
+		desc:'An item that iterates(moves incrementally) over a linear data structure.',
+		tag:'Data Structure'
+	},
+	{
+		word:'child',
+		desc:'Describes an item that is a descendant or sub-item of its parent.',
+		tags:['Data Structure','Trees']
+	},
+	{
+		word:'parent',
+		desc:'Describes an item that has a child.',
+		tags:['Data Structure','Trees']
+	},
+	{
+		words:[
+			'node',
+			'element'
+		],
+		desc:'A discrete piece of a structure.',
+		tags:['Data Structure','Trees']
+	},
+	{
+		word:'leaf node',
+		desc:'A node on a tree that has no children.',
+		tag:'Trees'
+	},
+	{
+		words:[
+			'root',
+			'root node',
+			'origin'
+		],
+		desc:'The root node of a tree, from which all other nodes descend.',
+		tag:'Trees'
+	},
+	{
+		word:'breadth-first',
+		desc:'A method of traversing a tree that involves moving across to neighboring nodes before descending to child nodes.',
+		tag:'Trees'
+	},
+	{
+		word:'depth-first',
+		desc:'A method of traversing a tree that involves descending as far as possible from the root node before backtracking and descending along the next path.',
+		tag:'Trees'
+	},
+	{
+		word:'collapse',
+		tag:'Trees'
+	},
+	{
+		word:['addr','address',null],
+		desc:'A location in memory.',
+		tag:'Memory'
+	},
+	{
+		word:['mem','memory',null],
+		desc:'A set range of slots that hold data.',
+		tag:'Memory'
+	},
+	{
+		word:['buf','buffer',null],
+		desc:'A piece of memory, usually allocated to hold a specific piece/size of data.',
+		tags:['Memory','Data Structure']
+	},
+	{
+		word:['str','string',null],
+		desc:'A set length of bytes/characters.',
+		tag:'Data Structure'
+	},
+	{
+		words:[
+			'print',
+			'output',
+			'display',
+			'write'
+		],
+		desc:'Output a piece of text to the display.',
+		tag:'Method Names'
+	},
+	{
+		word:'method',
+		desc:'A function that belongs to and acts primarily on an object or certain type of object.',
+		tag:'Objects'
+	},
+	{
+		word:'getter',
+		desc:'A method that returns a property of an object.',
+		tags:['Functions','Objects']
+	},
+	{
+		word:'setter',
+		desc:'A method that sets a property of an object.',
+		tags:['Functions','Objects']
+	},
+	{
+		word:'accessor',
+		desc:'A method that only accesses and does not modify an object.',
+		tags:['Functions','Objects']
+	},
+	{
+		word:'mutator',
+		desc:'A method that modifies an object.',
+		tags:['Functions','Objects']
+	},
+	{
+		words:[
+			'constructor',
+			'initiator'
+		],
+		desc:'A method called during/after the creation of a new object.',
+		tags:['Functions','Objects']
+	},
+	{
+		word:['init','initiate',null],
+		desc:'A method that initiates an object, usually called after its construction.',
+		tags:['Method Names','Objects']
+	},
+	{
+		words:[
+			'has',
+			'contains'
+		],
+		desc:'Returns whether or not the specified element(or index) is in a data structure.',
+		tag:'Method Names'
+	},
+	{
+		words:[
+			'add',
+			'append',
+			'push'
+		],
+		desc:'Adds an element to the end of a linear data structure.',
+		tag:'Method Names'
+	},
+	{
+		word:'insert',
+		desc:'Inserts an element to a certain position in a data structure.',
+		tag:'Method Names'
+	},
+	{
+		word:'pop',
+		desc:'Removes and returns the first element in a linear data structure.',
+		tag:'Method Names'
+	},
+	{
+		words:[
+			['rm','remove',null],
+			['del','delete',null]
+		],
+		desc:'Removes the element at the specified index from a linear data structure.',
+		tag:'Method Names'
+	},
+	{
+		words:[
+			'split',
+			'divide'
+		],
+		desc:'Splits a linear data structure into one or more pieces.',
+		tag:'Method Names'
+	},
+	{
+		words:[
+			'slice',
+			'splice'
+		],
+		desc:'Returns a piece of a linear data structure.',
+		tag:'Method Names'
+	},
+	{
+		words:[
+			'indexOf',
+			'find',
+			'getPosition'
+		],
+		desc:'Returns the index of a specified element in a data structure.',
+		tag:'Method Names'
+	},
+	{
+		word:'show',
+		desc:'Shows the object.',
+		tag:'Visual'
+	},
+	{
+		word:'hide',
+		desc:'Hides the object.',
+		tag:'Visual'
+	},
+	{
+		words:[
+			['a','alpha',null],
+			'opacity'
+		],
+		desc:'How transparent an object is.',
+		tag:'Visual'
+	},
+	{
+		word:'open',
+		tag:'Files'
+	},
+	{
+		word:'load',
+		tag:'Files'
+	},
+	{
+		word:'close',
+		tag:'Files'
+	},
+	{
+		word:'read',
+		tag:'Files'
+	},
+	{
+		word:'write',
+		tag:'Files'
+	},
+	{
+		word:'append',
+		desc:'Writes data to the end of the file.',
+		tag:'Files'
+	},
+	{
+		word:'truncate',
+		desc:'Writes data to the beginning of the file, overwriting its contents.',
+		tag:'Files'
+	},
+	{
+		word:'lock',
+		tag:'Files'
+	},
+	{
+		word:'unlock',
+		tag:'Files'
+	},
+	{
+		word:'touch',
+		desc:'Updates the timestamp to the current time.',
+		tag:'Files'
+	},
+	{
+		word:['eof','end-of-file',null],
+		tag:'Files'
+	},
+	{
+		words:[
+			'refresh',
+			'update',
+			['upgrade','Used moreso when an object itself rather than just its properties are changed']
+		],
+		tag:'Etc'
+	},
+	{
+		words:[
+			['del','delete',null],
+			'destroy',
+			'destruct',
+			'dispose',
+			'erase',
+			['rem','remove',null]
+		],
+		tag:'Etc'
+	},
+	{
+		words:[
+			'clean',
+			'clear',
+			'erase',
+			'free',
+			'purge',
+			'wipe'
+		],
+		tag:'Etc'
+	},
+	{
+		words:[
+			'join',
+			'merge',
+			['concat','concatenate','Usually associated with strings']
+		],
+		tag:'Etc'
+	},
+	{
+		words:[
+			'size',
+			['len','length',null],
+			['cnt','count',null]
+		],
+		tag:'Etc'
+	},
+	{
+		words:[
+			['pos','position',null],
+			'offset'
+		],
+		tag:'Etc'
+	},
+	{
+		word:['rand','random',null],
+		desc:'Returns a random number within a certain range.',
+		tags:['Method Names','Math']
+	},
+	{
+		word:'round',
+		desc:'Rounds the specified number to a certain amount.',
+		tags:['Method Names','Math']
+	},
+	{
+		word:'floor',
+		desc:'Truncates the fractional part of the specified number.',
+		tags:['Method Names','Math']
+	}
+];
---
+++ main.js
@@ -0,0 +1,361 @@
+// Todo:
+
+	// More entries!
+
+	// Selected entry count somewhere on page?
+
+	// Hierarchical tags?
+		// Only one level, selected the parent tag would also select its children
+
+
+// Some global vars
+var tagList = [],
+	selectedTag = null,
+	caseSensitiveSearch = false,
+	searchWordDesc = true;
+
+$(function(){
+
+	// Uncompress the dictionary
+	uncompressDictionary();
+
+	// Position the searchbox arrow
+	$('#searchBox').bind('keyup',refreshAllEntries);
+
+	// Hide the arrows
+	$('#searchArrow, #tagArrow').hide();
+
+	// Accumulate a list of all tags that appear in the entries
+	for(var i=0;i<dict.length;i++)
+		for(var tagIndex=0;tagIndex<dict[i].tags.length;tagIndex++)
+			if(tagList.indexOf(dict[i].tags[tagIndex]) < 0)
+				tagList.push(dict[i].tags[tagIndex]);
+
+	// Sort the tag list
+	tagList.sort();
+
+	// Add the event handler to the all-tags 
+	$('#tag-all').click(function(e){
+		// Remove the current selected tag class
+		$('.tagSelected').removeClass('tagSelected');
+		$(e.currentTarget).addClass('tagSelected');
+		// Clear the selected tag
+		selectedTag = null;
+		// Hide the arrow
+		hideTagArrow();
+		// Refresh the entries
+		refreshAllEntries();
+	});
+
+	// Create the tag list
+	var tagListP = $('#tagList')[0];
+
+	// Loop through the tags
+	for(var tagIndex=0;tagIndex<tagList.length;tagIndex++){
+
+		// Create the tag paragraph
+		var tagListEntry = document.createElement('p');
+		tagListEntry.className = 'tag';
+		tagListEntry.id = 'tag-'+tagIndex;
+		$(tagListEntry).text(tagList[tagIndex]);
+		tagListP.appendChild(tagListEntry);
+
+		// Create a click handler to set/unset the tag
+		$(tagListEntry).click(
+			function(e){
+				// Remove the current selected tag class
+				$('.tagSelected').removeClass('tagSelected');
+				// Get the tag index
+				var index = e.currentTarget.id.split('-')[1];
+				// Check if enabled/disabled
+				if(tagList[index] === selectedTag){
+					// Clear the selected tag
+					selectedTag = null;
+					// Hide the arrow
+					hideTagArrow();
+				}
+				else{
+					// Set the selected tag class
+					e.currentTarget.className = 'tag tagSelected';
+					selectedTag = tagList[index];
+					// Show the tag arrow
+					showTagArrow(e.currentTarget);
+				}
+				// Refresh the entries
+				refreshAllEntries();
+			}
+		);
+
+		// Create a div for the entries
+
+		var entrySection = document.createElement('div');
+		entrySection.className = 'entrySection';
+		entrySection.id = 'entrySection-'+tagIndex;
+		$('#entries')[0].appendChild(entrySection);
+
+		// Create a header in the entry list
+
+		var entrySectionHeader = document.createElement('p');
+		entrySectionHeader.className = 'entrySectionHeader';
+		$(entrySectionHeader).text(tagList[tagIndex]);
+		entrySection.appendChild(entrySectionHeader);
+
+		// Loop through the entries
+		for(var i=0;i<dict.length;i++)
+
+			//Check if the entry has the current tag
+			if(dict[i].tags.indexOf(tagList[tagIndex]) > -1){
+
+				// Cache the entry
+				var entry = dict[i];
+	
+				// Create the entry block
+				var block = document.createElement('div');
+				block.className = 'entryBlock';
+				block.id = "entry"+i;
+				entrySection.appendChild(block);
+		
+				// Create the word list for the entry
+				var wordList = document.createElement('ul');
+				wordList.className = 'entryWordList';
+				block.appendChild(wordList);
+		
+				// Loop through the words
+				for(var j=0;j<entry.words.length;j++){
+	
+					var word = entry.words[j];
+	
+					var wordLi = document.createElement('li');
+					wordLi.className = 'entryWord';
+					wordList.appendChild(wordLi);
+	
+					// Loop through the subwords
+					for(var subWordIndex=0;subWordIndex<word.length-1;subWordIndex++){
+						var subWordSpan = document.createElement('span');
+						subWordSpan.className = 'entrySubWord';
+						$(subWordSpan).text(word[subWordIndex]);
+						if(subWordIndex < word.length-2)
+							$(subWordSpan).text($(subWordSpan).text()+', ');
+						wordLi.appendChild(subWordSpan);
+					}
+	
+					if(typeof word[word.length-1] === 'string'){
+		
+						var wordDesc = document.createElement('p');
+						wordDesc.className = 'entryWordDesc';
+						$(wordDesc).text(word[word.length-1]);
+						wordLi.appendChild(wordDesc);
+		
+					}
+	
+				}
+	
+				// Add the description if there is one
+				if(entry.desc.length){
+					var desc = document.createElement('p');
+					desc.className = 'entryDesc';
+					$(desc).text(entry.desc);
+					block.appendChild(desc);
+				}
+
+			}
+
+	}
+
+});
+
+// Searching
+
+function refreshAllEntries(){
+	// Check if any tag is selected
+	if(selectedTag !== null){
+		// Yes, hide all but the selected tag
+		$('.entrySection').hide();
+		$('#entrySection-'+tagList.indexOf(selectedTag)).show();
+	}
+	else{
+		// No, show every tag section
+		$('.entrySection').show();
+	}
+	// Unhighlight any highlighted words
+	unhighlightAll();
+	// Check if filtering entries
+	if($('#searchBox').val()){
+		// Show the search arrow
+		showSearchArrow();
+		// Hide all the sections
+		$('.entrySection').hide();
+		// Hide all the entries
+		hideAll();
+		// Show the entries
+		showSearchedEntries();
+	}
+	else{
+		// Hide the search arrow
+		hideSearchArrow();
+		// Show all the entries
+		showAll();
+	}
+}
+
+function showSearchedEntries(){
+	// Get the filter text
+	var filter = $('#searchBox').val();
+	// Keep a list of visible sections
+	var visibleTags = [];
+	// Loop through the entries
+	for(var i=0;i<dict.length;i++){
+		// Cache the entry
+		var entry = dict[i];
+		// Check that the entry is tagged
+		if(isEntryTagged(entry)){
+			// Keep a flag to check if the entry is visible
+			entryVisible = false;
+			// Loop through each word in the entry
+			for(var wordIndex=0;wordIndex<entry.words.length;wordIndex++){
+				// Check if the word is visible
+				if(isWordVisible(entry.words[wordIndex],filter)){
+					// Highlight the word and set the entry as visible
+					$($('#entry'+i)[0].children[0].children[wordIndex]).addClass('entryWordHighlighted');
+					entryVisible = true;
+				}
+			}
+			// Check if the entry is visible
+			if(entryVisible){
+				// Show the entry and add the tags to the visible tag list
+				showEntry(i);
+				for(var tagI=0;tagI<entry.tags.length;tagI++){
+					if(visibleTags.indexOf(tagList.indexOf(entry.tags[tagI])) < 0){
+						visibleTags.push(entry.tags[tagI]);
+					}
+				}
+			}
+		}
+	}
+	// Show the sections for the visible tags
+	for(var tagI=0;tagI<visibleTags.length;tagI++)
+		$('#entrySection-'+tagList.indexOf(visibleTags[tagI])).show();
+}
+
+function isEntryTagged(entry){
+	// Return true if all tags are selected
+	if(selectedTag === null)
+		return true;
+	// Else loop through the entry's tags
+	for(var i=0;i<entry.tags.length;i++)
+		// Return true if it matches the selected tag
+		if(selectedTag === entry.tags[i])
+			return true;
+	// Else no matches, return false
+	return false;
+}
+
+function isWordVisible(word,filter){
+	// Loop though each word, plus 1 for the description
+	for(var i=0;i<word.length-!searchWordDesc;i++)
+		// Check if its a string, it might be null for no word desc
+		if(typeof word[i] === 'string')
+			// Return true if its visible per the search
+			if(isStringVisible(word[i],filter))
+				return true;
+	// Else no matches, return false
+	return false;
+}
+
+function isStringVisible(string,filter){
+	// Use indexOf if case sensitive, else use a case insensitive RegExp
+	return (caseSensitiveSearch ? string.indexOf(filter) : string.search(new RegExp(filter,'i'))) > -1;
+}
+
+function unhighlightAll(){
+	// Remove the highlighted class
+	$('.entryWordHighlighted').removeClass('entryWordHighlighted');
+}
+
+// Arrow
+
+function showSearchArrow(){
+	// Get the sidebar element
+	var sidebar = $('#sidebar')[0];
+	// Position and show the arrow
+	var searchBox = $('#searchBox');
+	var searchArrow = $('#searchArrow');
+	searchArrow[0].style.left = sidebar.offsetLeft+sidebar.offsetWidth-16+'px';
+	searchArrow[0].style.top = searchBox[0].offsetTop+searchBox[0].offsetHeight/2-16+'px';
+	searchArrow.show();
+}
+
+function hideSearchArrow(){
+	$('#searchArrow').hide();
+}
+
+function showTagArrow(target){
+	// Get the sidebar element
+	var sidebar = $('#sidebar')[0];
+	// Position and show the arrow
+	var arrow = $('#tagArrow');
+	arrow[0].style.top = target.offsetTop+target.offsetHeight/2-16+'px';
+	arrow[0].style.left = sidebar.offsetLeft+sidebar.offsetWidth-16+'px';
+	arrow.show();
+}
+
+function hideTagArrow(){
+	$('#tagArrow').hide();
+}
+
+// Entry Hiding/Showing
+
+function showEntry(index){
+	$('#entry'+index).show();
+}
+
+function hideEntry(index){
+	$('#entry'+index).hide();
+}
+
+function hideAll(){
+	$('.entryBlock').hide();
+}
+
+function showAll(){
+	$('.entryBlock').show();
+}
+
+// Dictionary manipulation
+
+function uncompressDictionary(){
+	// Loop through the entries
+	for(var i=0;i<dict.length;i++){
+		// Cache the entry
+		var entry = dict[i];
+		// Expand a single word into a subword array
+		if(typeof entry.word === 'string'){
+			entry.words = [[entry.word,null]];
+			delete entry.word;
+		}
+		// Expand a single word array into a subword array
+		else if(typeof entry.word === 'object'){
+			entry.words = [entry.word];
+			delete entry.word;
+		}
+		// Add null descriptions to single subwords and expand single words
+		else{
+			for(var wordI=0;wordI<entry.words.length;wordI++)
+				if(typeof entry.words[wordI] === 'string')
+					entry.words[wordI] = [entry.words[wordI],null];
+				else if(entry.words[wordI].length === 1)
+					entry.words[wordI].push(null);
+		}
+		// Expand a single tag into an array
+		if(typeof entry.tag === 'string'){
+			entry.tags = [entry.tag];
+			delete entry.tag;
+		}
+		// Set an empty tags if undefined
+		else if(typeof entry.tags === 'undefined')
+			entry.tags = [];
+		// Set an empty description if undefined
+		if(typeof entry.desc === 'undefined')
+			entry.desc = '';
+	}
+}
